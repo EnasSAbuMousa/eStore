ad8db569ce62021929af51e8c137bdc26dd81b4c
EnasSAbuMousa <enassabumousa@gmail.com>
2025-02-16 21:03:47 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,31 @@ Route::post(uri: '/about', action: function () {
 });
 
 Route::get(uri: 'tasks' , action: function(){
-
-     return view('tasks');
+     $tasks = DB::table('tasks')->get();
+     return view('tasks', compact('tasks'));
 });
 
 Route::post(uri: '/create', action: function () {
     $task_name= $_POST['name'];
     DB::table('tasks')->insert(['name' => $task_name]);
-    return view(view: 'tasks');
+    return redirect()->back();
+});
+
+Route::post(uri: '/delete/{id}', action: function ($id) {
+    DB::table('tasks')->where('id' , $id)->delete();
+    return redirect()->back();
+});
+
+Route::post(uri: '/edit/{id}', action: function ($id) {
+   $task = DB::table('tasks')->where('id' , $id)->first();
+   $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('task','tasks'));
+});
+
+Route::post(uri:'update', action: function(){
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id' , '=', $id)->update(['name' => $_POST['name']]);
+    return redirect('tasks');
 });
 
 
